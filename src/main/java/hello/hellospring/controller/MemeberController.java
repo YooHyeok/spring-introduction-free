5cf91459ac6deab4f50e62a973455fac742b504e
YooHyeok <yjk7454@naver.com>
2023-02-15 22:36:33 +0900

    회원가입 등록 기능 및 저장 구현 / 회원가입 페이지구현

--- src/main/java/hello/hellospring/controller/MemeberController.java
+++ src/main/java/hello/hellospring/controller/MemeberController.java
@@ -1,8 +1,13 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.domain.MemberForm;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemeberController {
@@ -12,4 +17,20 @@ public class MemeberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("members/new")
+//    public String create(@RequestParam(name = "name") String parameter) { //@RequestParam은 주로 변수명 변경, required 옵션변경시 사용한다.
+    public String create(@ModelAttribute MemberForm form) {
+        //@ModelAttribute 또한 name옵션을 통해 변수명 변경시 사용, thymeleaf에서 th:object 속성과 매칭시켜 변수명을 변경할때 사용한다.
+        // JSP에서는 form TagLib을 사용하여 spring4 - commandName 혹은 spring5 - modelAttribute 속성에 지정해 줄 수 있다.
+        Member member = new Member();
+        member.setName(form.getName());
+        System.out.println(member.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
 }
---
+++ src/main/java/hello/hellospring/domain/MemberForm.java
@@ -0,0 +1,21 @@
+package hello.hellospring.domain;
+
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberForm{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
