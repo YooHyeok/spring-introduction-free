51ce7c94a962330e1bec0ba6286c7da1d7dbdbdd
YooHyeok <yjk7454@naver.com>
2023-02-17 18:24:13 +0900

    SpringDataJPA 적용 및 테스트

--- src/main/java/hello/hellospring/SpringBeanConfig.java
+++ src/main/java/hello/hellospring/SpringBeanConfig.java
@@ -15,24 +15,32 @@ public class SpringBeanConfig {
 
     private final DataSource dataSource;
     private final EntityManager entityManager;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringBeanConfig(DataSource dataSource, EntityManager entityManager) {
+    public SpringBeanConfig(DataSource dataSource, EntityManager entityManager, MemberRepository memberRepository) {
         this.dataSource = dataSource;
         this.entityManager = entityManager;
+        this.memberRepository = memberRepository;
     }
 
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+//        return new MemberService(memberRepository()); // 임시DB, 순수JDBC, JDBCTemplate, EntityManager
+        return new MemberService(memberRepository); // 인터페이스 주입 SpringDataJpaMemberRepository가 구현하면서 Spring Data JPA의 기능을 사용할수 있게 된다.
     }
 
+    /**
+     * 임시DB, 순수JDBC, JDBCTemplate, EntityManager 주입
+     * @return
+     */
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcPureMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
         return new EntityManagerJpaMemberRepository(entityManager);
+
     }
 }
---
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * MemberRepository의 추상메소드를 다시 추상메소드로 오버라이딩하면서 해당 메소드를 JpaRepository를 통해 SpringDataJPA로 구현
+ */
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long> , MemberRepository{
+    @Override
+    Optional<Member> findByName(String name);
+}
