7bf6e91f7b42449e65fb51f42030c4f51f461b9b
YooHyeok <yjk7454@naver.com>
2023-02-17 19:11:13 +0900

    recommit

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -9,6 +9,9 @@ import java.util.Optional;
  * MemberRepository의 추상메소드를 다시 추상메소드로 오버라이딩하면서 해당 메소드를 JpaRepository를 통해 SpringDataJPA로 구현
  */
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long> , MemberRepository{
+
+    //JPQL : select m from Member m where m.name=:name
     @Override
     Optional<Member> findByName(String name);
+//    Optional<Member> findByMemberMNoAndName(Long id, String name);
 }
